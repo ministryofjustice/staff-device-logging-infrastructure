1f612901a3dcd5a679c2bbcc359d792b2c66f696
David Capper <david.capper@madetech.com>
2020-06-24 14:55:50 +0100

    Create an http POST logs route

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -9,12 +9,12 @@ resource "aws_api_gateway_rest_api" "logging_gateway" {
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.logging_gateway.id
   parent_id   = aws_api_gateway_rest_api.logging_gateway.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "logs"
 }
 
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.logging_gateway.id
   resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
+  http_method   = "POST"
   authorization = "NONE"
 }
