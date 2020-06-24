810a38ef00e35a41795d61f2fb237d6c56b66fb0
David Capper <david.capper@madetech.com>
2020-06-24 14:50:29 +0100

    Create a route on the API gateway

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -5,3 +5,16 @@ resource "aws_api_gateway_rest_api" "logging_gateway" {
     types = ["REGIONAL"]
   }
 }
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.logging_gateway.id
+  parent_id   = aws_api_gateway_rest_api.logging_gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.logging_gateway.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
