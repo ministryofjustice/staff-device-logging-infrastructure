9dca80dc91bafde82976bab1bc50727d255e153e
David Capper <david.capper@madetech.com>
2020-06-25 10:54:27 +0100

    provide api gateway integration with role

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -26,6 +26,29 @@ resource "aws_api_gateway_integration" "sqs-integration" {
   type = "AWS"
   integration_http_method = "POST"
   uri = "arn:aws:apigateway:eu-west-2:sqs:path/${aws_sqs_queue.custom_log_queue.name}"
+  credentials = aws_iam_role.custom-logging-sqs-write-role
+}
+
+resource "aws_iam_role" "custom-logging-sqs-write-role" {
+  assume_role_policy = data.aws_iam_policy_document.custom-logging-sqs-write-role-assume-policy
+  permissions_boundary = ""
+}
+
+resource "aws_iam_role_policy_attachment" "sqs-send-message-custom-logging-attachment" {
+  policy_arn = aws_iam_policy.api-gateway-sqs-send-msg-policy.arn
+  role = aws_iam_role.custom-logging-sqs-write-role.arn
+}
+
+data "aws_iam_policy_document" "custom-logging-sqs-write-role-assume-policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    resources = [
+      aws_api_gateway_rest_api.logging_gateway.arn
+    ]
+  }
 }
 
 # following this blog post to create a queue which is written to by the api gateway
