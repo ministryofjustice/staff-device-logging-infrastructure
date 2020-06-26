361936ef84262fd19f19e2b8007f8c5af4f410bd
Emile <emile.swarts@digital.justice.gov.uk>
2020-06-26 14:02:36 +0100

    Allow remote VPC DNS resolution for the peering connection

--- modules/logging/vpcPeering.tf
+++ modules/logging/vpcPeering.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc_peering_connection" "ost_logging_vpc" {
-  peer_owner_id = var.ost_aws_account_id
-  peer_vpc_id   = var.ost_vpc_id
-  vpc_id        = var.vpc_id
+  peer_owner_id                   = var.ost_aws_account_id
+  peer_vpc_id                     = var.ost_vpc_id
+  vpc_id                          = var.vpc_id
+  allow_remote_vpc_dns_resolution = true
 }
