b82502590d24661e2fb482f1a6c631dfb66851ee
Emile <emile.swarts@digital.justice.gov.uk>
2020-06-29 15:56:36 +0100

    Use Private subnets instead of public subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,7 +14,7 @@ module "vpc" {
     "${var.region}c"
   ]
 
-  public_subnets = [
+  private_subnets = [
     cidrsubnet(var.cidr_block, 8, 1),
     cidrsubnet(var.cidr_block, 8, 2),
     cidrsubnet(var.cidr_block, 8, 3)
