92c92b86e2211574de12ca33d1eeb351f5826959
Emile <emile.swarts@digital.justice.gov.uk>
2020-06-30 16:23:47 +0100

    Be explicit about one gateway per AZ

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,9 +1,10 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.28.0"
-  name = "${var.prefix}-vpc"
+  name = var.prefix
 
   cidr = var.cidr_block
+  one_nat_gateway_per_az = true
 
    azs = [
      "${var.region}a",
@@ -11,15 +12,15 @@ module "vpc" {
      "${var.region}c"
    ]
   private_subnets = [
-    cidrsubnet(var.cidr_block, 8, 1), # 10.0.1.0/24
-    cidrsubnet(var.cidr_block, 8, 2), # 10.0.2.0/24
-    cidrsubnet(var.cidr_block, 8, 3)  # 10.0.3.0/24
+    cidrsubnet(var.cidr_block, 8, 1), # 10.193.1.0/24
+    cidrsubnet(var.cidr_block, 8, 2), # 10.193.2.0/24
+    cidrsubnet(var.cidr_block, 8, 3)  # 10.193.3.0/24
   ]
 
   public_subnets = [
-    cidrsubnet(var.cidr_block, 8, 5),
-    cidrsubnet(var.cidr_block, 8, 6),
-    cidrsubnet(var.cidr_block, 8, 7) 
+    cidrsubnet(var.cidr_block, 8, 5), # 10.193.5.0/24
+    cidrsubnet(var.cidr_block, 8, 6), # 10.193.6.0/24
+    cidrsubnet(var.cidr_block, 8, 7)  # 10.193.7.0/24
   ]
 
   enable_nat_gateway = true
