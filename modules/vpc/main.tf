10ff1ac0585c595d4cc631a25e006138fc2db2be
Emile <emile.swarts@digital.justice.gov.uk>
2020-06-30 08:02:47 +0100

    Create at least one public subnet per AZ
    This module fails if you don't have enough public subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,22 +4,26 @@ module "vpc" {
 
   name = var.prefix
 
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  cidr                 = var.cidr_block
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+  cidr                   = var.cidr_block
 
   azs = [
     "${var.region}a",
-    "${var.region}b",
-    "${var.region}c"
+    "${var.region}b"
   ]
 
   public_subnets = [
-    cidrsubnet(var.cidr_block, 8, 1)
+    cidrsubnet(var.cidr_block, 8, 1),
+    cidrsubnet(var.cidr_block, 8, 2)
   ]
 
   private_subnets = [
-    cidrsubnet(var.cidr_block, 8, 2)
+    cidrsubnet(var.cidr_block, 8, 3),
+    cidrsubnet(var.cidr_block, 8, 4)
   ]
 
   map_public_ip_on_launch = false
