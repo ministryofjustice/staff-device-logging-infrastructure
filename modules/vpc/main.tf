319fc77ab74eec0e83894cda64aa6ff7f7a008d8
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-01 09:47:20 +0100

    Update AZs and force NAT Gateway per AZ

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -24,9 +24,11 @@ module "vpc" {
   #   cidrsubnet(var.cidr_block, 8, 6)  # 10.193.6.0/24
   # ]
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   private_subnets = ["10.193.1.0/24", "10.193.2.0/24", "10.193.3.0/24"]
   public_subnets  = ["10.193.4.0/24", "10.193.5.0/24", "10.193.6.0/24"]
 
   enable_nat_gateway = true
+  one_nat_gateway_per_az = true
+  single_nat_gateway = false
 }
