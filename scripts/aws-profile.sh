6d9e881a2cdf12a6fe729cfba6dc99253ee944a1
cob16 <public+github@cormacbrady.info>
2020-07-08 11:44:51 +0100

    set a profile name for assume role

--- scripts/aws-profile.sh
+++ scripts/aws-profile.sh
@@ -7,7 +7,8 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 ASSUME_ROLE_ARN=$1
-BUILD_ID=${2:-"test-session"}
+PROFILE_NAME=$2
+BUILD_ID=${3:-"test-session"}
 echo "role-session-name will be: $BUILD_ID"
 
 echo 'testing current credentials...'
@@ -18,10 +19,10 @@ echo 'testing we are allowed to assume the given role...'
 aws sts assume-role --role-arn "$ASSUME_ROLE_ARN" --role-session-name "$BUILD_ID-setup-test" --duration-seconds 900 > /dev/null
 echo 'Success'
 
-echo "writing default profile that will assume role: to $ASSUME_ROLE_ARN ..."
+echo "writing profile '$PROFILE_NAME' that will assume role: to $ASSUME_ROLE_ARN ..."
 
 mkdir -p  ~/.aws
-echo "[default]" >> ~/.aws/config
+echo "[profile $PROFILE_NAME]" >> ~/.aws/config
 echo "role_arn = $ASSUME_ROLE_ARN" >> ~/.aws/config
 echo "session_name = $BUILD_ID" >> ~/.aws/config
 echo "credential_source = EcsContainer" >> ~/.aws/config
