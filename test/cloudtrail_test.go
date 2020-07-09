15b75845b27aceafa0b07b113df8b64e7c47fe82
Scott Edwards <scotteza@gmail.com>
2020-07-09 16:46:44 +0100

    Add retry loop to CloudWatch+CloudTrail test
    Co-authored-by: Caitlin <caitlin@madetech.com>

--- test/cloudtrail_test.go
+++ test/cloudtrail_test.go
@@ -9,12 +9,12 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go/service/s3"
-    "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
 )
 
 const retryDelay2 = time.Second * 5
@@ -60,19 +60,35 @@ func VerifyThatAMessageAppearedInACloudWatchLogGroup(thisTest testInfo) {
 	logStream := accountNumber + "_CloudTrail_" + testRegion2
 	logGroup := terraform.Output(thisTest.instance, thisTest.config, "log_group_name")
 
-
 	sess, _ := session.NewSession(&aws.Config{Region: aws.String(testRegion2)})
 
 	svc := cloudwatchlogs.New(sess)
 
-    resp, err := svc.GetLogEvents(&cloudwatchlogs.GetLogEventsInput{
-        Limit:         aws.Int64(100),
-        LogGroupName:  aws.String(logGroup),
-        LogStreamName: aws.String(logStream),
-    })
+	maxRetries := 12 // One minute with 5 second sleeps
 
-	assert.NoError(thisTest.instance, err)
-	assert.Len(thisTest.instance, resp.Events, 0, "***No CloudWatch messages received***")
+	messagesReceived := false
+	for i := 0; i <= maxRetries; i++ {
+
+		resp, err := svc.GetLogEvents(&cloudwatchlogs.GetLogEventsInput{
+			Limit:         aws.Int64(100),
+			LogGroupName:  aws.String(logGroup),
+			LogStreamName: aws.String(logStream),
+		})
+
+		assert.NoError(thisTest.instance, err)
+
+		eventsLength := len(resp.Events)
+
+		if eventsLength > 0 {
+			fmt.Printf("Received %v CloudWatch events from Cloudtrail\n", eventsLength)
+			messagesReceived = true
+			break
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+
+	assert.True(thisTest.instance, messagesReceived, "***Received no CloudWatch log messages from CloudTrail***")
 }
 
 // TODO: either make test info globally scoped, or create a local version here
