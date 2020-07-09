b834cacbfc2c33cbce07a4455081b9f7a64c6d34
cob16 <public+github@cormacbrady.info>
2020-07-09 14:48:56 +0100

    use backticks in subshell cmd
    causes an error when run in sh

--- scripts/deploy_functionbeat.sh
+++ scripts/deploy_functionbeat.sh
@@ -15,8 +15,8 @@ zip -u package-aws.zip moj.crt moj.key elk-ca.crt
 ./functionbeat -e -c ../functionbeat-config.yml export "function pttp-$ENV-infra-cloudwatch" > "cf-$ENV-cloudwatch.json"
 ./functionbeat -e -c ../functionbeat-config.yml export "function pttp-$ENV-infra-sqs"        > "cf-$ENV-sqs.json"
 
-CW_S3_KEY=$(cat cf-$ENV-cloudwatch.json | jq -r '.. |."S3Key"? | select(. != null)')
-SQS_S3_KEY=$(cat cf-$ENV-sqs.json       | jq -r '.. |."S3Key"? | select(. != null)')
+CW_S3_KEY=`cat cf-$ENV-cloudwatch.json | jq -r '.. |."S3Key"? | select(. != null)'`
+SQS_S3_KEY=`cat cf-$ENV-sqs.json       | jq -r '.. |."S3Key"? | select(. != null)'`
 
 aws s3 cp --no-progress ./package-aws.zip "s3://pttp-$ENV-infra-functionbeat-artifacts/$CW_S3_KEY"
 aws s3 cp --no-progress ./package-aws.zip "s3://pttp-$ENV-infra-functionbeat-artifacts/$SQS_S3_KEY"
