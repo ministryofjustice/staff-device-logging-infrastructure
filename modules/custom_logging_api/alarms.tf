5c25380bca5a60639e1a83ec491ac4d90f1032a5
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-09 15:06:31 +0100

    Create CloudWatch metrics for Lambda

--- modules/custom_logging_api/alarms.tf
+++ modules/custom_logging_api/alarms.tf
@@ -195,3 +195,218 @@ resource "aws_cloudwatch_metric_alarm" "logging-api-gateway-latency" {
   alarm_description  = "This alarm monitors the latency"
   treat_missing_data = "breaching"
 }
+
+# lambda Invocations <= 1 / 1m
+resource "aws_cloudwatch_metric_alarm" "logging-api-lambda-invocations-cloudwatch" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-invocations-cloudwatch"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Count"
+  namespace           = "AWS/Lambda"
+  statistic           = "Minimum"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-cloudwatch"
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda invocations for CloudWatch data source"
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "logging-api-lambda-invocations-sqs" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-invocations-sqs"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Count"
+  namespace           = "AWS/Lambda"
+  statistic           = "Minimum"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-sqs"
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda invocations for SQS data source"
+  treat_missing_data = "breaching"
+}
+
+# # lambda Errors > 0 / 1m
+
+resource "aws_cloudwatch_metric_alarm" "logging-api-lambda-errors-cloud-watch" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-errors-cloud-watch"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  statistic           = "Average"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-cloudwatch"
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda Errors for CloudWatch data source"
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "logging-api-lambda-errors-sqs" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-errors-sqs"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  statistic           = "Average"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-sqs"
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda Errors for SQS data source"
+  treat_missing_data = "breaching"
+}
+
+
+# lambda DeadLetterErrors (TBC)
+
+# lambda DestinationDeliveryFailures > 0 / 1m
+
+resource "aws_cloudwatch_metric_alarm" "logging-api-lambda-destination-delivery-failures-cloudwatch" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-destination-delivery-failures-cloudwatch"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Count"
+  namespace           = "AWS/Lambda"
+  statistic           = "Minimum"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-cloudwatch"
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda destination delivery errors for Cloudwatch data source"
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "logging-api-lambda-destination-delivery-failures-sqs" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-destination-delivery-failures-sqs"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Count"
+  namespace           = "AWS/Lambda"
+  statistic           = "Minimum"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-sqs"
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda destination delivery errors for SQS data source"
+  treat_missing_data = "breaching"
+}
+
+
+# lambda Throttles > 0 / 1m
+
+resource "aws_cloudwatch_metric_alarm" "logging-lambda-throttles-cloudwatch" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-throttle-count-cloudwatch"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Count"
+  namespace           = "AWS/Lambda"
+  statistic           = "Minimum"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-cloudwatch"
+
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda throttles for CloudWatch data source"
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "logging-lambda-throttles-sqs" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-throttle-count-sqs"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Count"
+  namespace           = "AWS/Lambda"
+  statistic           = "Minimum"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-sqs"
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda throttles for SQS data source"
+  treat_missing_data = "breaching"
+}
+
+# # lambda ProvisionedConcurrencySpilloverInvocations > 0 / 1m
+
+resource "aws_cloudwatch_metric_alarm" "logging-lambda-concurrency-spillover-invocations-cloudwatch" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-provisioned-concurrency-spillover-count-cloudwatch"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Count"
+  namespace           = "AWS/Lambda"
+  statistic           = "Minimum"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-cloudwatch"
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda provisioned concurrency spillover invocations Cloud Watch data source "
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "logging-lambda-concurrency-spillover-invocations-sqs" {
+  alarm_name          = "${var.prefix}-custom-logs-lambda-provisioned-concurrency-spillover-count-sqs"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "1"
+  metric_name         = "Count"
+  namespace           = "AWS/Lambda"
+  statistic           = "Minimum"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = "${var.prefix}-infra-sqs"
+  }
+
+  alarm_actions = [var.sns_topic_arn]
+
+  alarm_description = "This alarm monitors the the number of Lambda provisioned concurrency spillover invocations SQS data source "
+  treat_missing_data = "breaching"
+}
+
