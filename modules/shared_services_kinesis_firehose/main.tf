1f6d92ea1bba2e965a592ada3b068310f49b25e4
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-10 16:38:05 +0100

    Encrypt Kinesis stream

--- modules/shared_services_kinesis_firehose/main.tf
+++ modules/shared_services_kinesis_firehose/main.tf
@@ -1,10 +1,17 @@
 resource "aws_kinesis_stream" "shared_services_destination_stream" {
   name             = "${var.prefix}-shared-services-log-destinion-stream"
   shard_count      = 1
+  encryption_type  = "KMS"
+  kms_key_id       = aws_kms_key.kinesis_stream_key.id
   retention_period = 48
 
   shard_level_metrics = [
     "IncomingBytes",
     "OutgoingBytes",
   ]
+}
+
+resource "aws_kms_key" "kinesis_stream_key" {
+  description             = "${var.prefix}-shared-services-kinesis-stream-key"
+  deletion_window_in_days = 10
 }
