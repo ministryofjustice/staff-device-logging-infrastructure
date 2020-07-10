fb954b684c82a237117e428de470241da193e481
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-10 14:30:57 +0100

    Restrict Security Group permissions for Beats
    Remove commented code for test instance

--- modules/logging/beats.tf
+++ modules/logging/beats.tf
@@ -108,3 +108,22 @@ resource "aws_iam_role" "beats-lambda-role" {
 }
 EOF
 }
+
+resource "aws_security_group" "functionbeats" {
+  name   = "${var.prefix}-functionbeats"
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
