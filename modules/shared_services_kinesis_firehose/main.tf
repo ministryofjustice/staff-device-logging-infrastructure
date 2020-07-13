4ba01893a247f091b52bba5bd4537dc68794bd59
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-13 12:20:55 +0100

    Create destination for shared services logs on trusting account

--- modules/shared_services_kinesis_firehose/main.tf
+++ modules/shared_services_kinesis_firehose/main.tf
@@ -15,3 +15,9 @@ resource "aws_kms_key" "kinesis_stream_key" {
   description             = "${var.prefix}-shared-services-kinesis-stream-key"
   deletion_window_in_days = 10
 }
+
+resource "aws_cloudwatch_log_destination" "log-forward-to-kinesis-arn" {
+  name       = "${var.prefix}-log-forward-to-kinesis-arn"
+  role_arn   = "${aws_iam_role.kinesis-cloudwatch-subscription-role.arn}"
+  target_arn = "${aws_kinesis_stream.shared_services_destination_stream.arn}"
+}
