865666d5d52c468442d5b72c4388aee54e2e6ee3
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-13 14:05:56 +0100

    Comment out destination for now
    Unsure why this doesn't work, get the pipeline green again for further
    debugging

--- modules/shared_services_kinesis_firehose/main.tf
+++ modules/shared_services_kinesis_firehose/main.tf
@@ -16,11 +16,11 @@ resource "aws_kms_key" "kinesis_stream_key" {
   deletion_window_in_days = 10
 }
 
-resource "aws_cloudwatch_log_destination" "log-forward-to-kinesis" {
-  name       = "${var.prefix}-log-forward-to-kinesis"
-  role_arn   = "${aws_iam_role.kinesis-cloudwatch-subscription-role.arn}"
-  target_arn = "${aws_kinesis_stream.shared_services_destination_stream.arn}"
-}
+# resource "aws_cloudwatch_log_destination" "log-forward-to-kinesis" {
+#   name       = "${var.prefix}-log-forward-to-kinesis"
+#   role_arn   = "${aws_iam_role.kinesis-cloudwatch-subscription-role.arn}"
+#   target_arn = "${aws_kinesis_stream.shared_services_destination_stream.arn}"
+# }
 
 # resource "aws_iam_policy" "kinesis-cloudwatch-subscription-destination-policy" {
 #   name = "${var.prefix}-kinesis-cloudwatch-subscription-destination-policy"
@@ -30,13 +30,13 @@ resource "aws_cloudwatch_log_destination" "log-forward-to-kinesis" {
 #     Version = "2012-10-17",
 #     Statement = [
 #       {
-#       "Sid" : "",
-#       "Effect" : "Allow",
-#       "Principal" : {
-#           "AWS" : var.shared_services_account_arn
-#       },
-#       "Action" : "logs:PutSubscriptionFilter",
-#       "Resource" : "${aws_cloudwatch_log_destination.log-forward-to-kinesis.arn}"
+#         "Sid" : "",
+#         "Effect" : "Allow",
+#         "Principal" : {
+#             "AWS" : var.shared_services_account_arn
+#         },
+#         "Action" : "logs:PutSubscriptionFilter",
+#         "Resource" : "${aws_cloudwatch_log_destination.log-forward-to-kinesis.arn}"
 #       }
 #     ]
 #   })
