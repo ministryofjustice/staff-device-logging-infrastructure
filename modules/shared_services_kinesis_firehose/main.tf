3143af02592e4d04a20112a9966869c52711bba0
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-13 14:40:29 +0100

    Test whether encryption requires different IAM policy for subscriptions

--- modules/shared_services_kinesis_firehose/main.tf
+++ modules/shared_services_kinesis_firehose/main.tf
@@ -1,8 +1,8 @@
 resource "aws_kinesis_stream" "shared_services_destination_stream" {
   name             = "${var.prefix}-shared-services-log-destinion-stream"
   shard_count      = 1
-  encryption_type  = "KMS"
-  kms_key_id       = aws_kms_key.kinesis_stream_key.id
+#   encryption_type  = "KMS"
+#   kms_key_id       = aws_kms_key.kinesis_stream_key.id
   retention_period = 48
 
   shard_level_metrics = [
@@ -15,6 +15,47 @@ resource "aws_kms_key" "kinesis_stream_key" {
   description             = "${var.prefix}-shared-services-kinesis-stream-key"
   deletion_window_in_days = 10
 }
+
+resource "aws_iam_role" "kinesis-cloudwatch-subscription-role" {
+  name = "${var.prefix}-kinesis-cloudwatch-subscription-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        "Effect": "Allow",
+        "Principal": { "Service": "logs.${var.region}.amazonaws.com" },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "kinesis-cloudwatch-subscription-policy" {
+  name = "${var.prefix}-kinesis-cloudwatch-subscription-policy"
+  path = "/"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        "Effect": "Allow",
+        "Action": "kinesis:PutRecord",
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": "iam:PassRole",
+        "Resource": "${aws_iam_role.kinesis-cloudwatch-subscription-role.arn}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild-attachment" {
+  role       = aws_iam_role.kinesis-cloudwatch-subscription-role.name
+  policy_arn = aws_iam_policy.kinesis-cloudwatch-subscription-policy.arn
+}
+
 
 # resource "aws_cloudwatch_log_destination" "log-forward-to-kinesis" {
 #   name       = "${var.prefix}-log-forward-to-kinesis"
