d35820dce27490468dd5a3741e7547ced5619666
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-14 14:17:14 +0100

    Re-enable encryption for Kinesis

--- modules/shared_services_kinesis_firehose/main.tf
+++ modules/shared_services_kinesis_firehose/main.tf
@@ -1,13 +1,13 @@
 resource "aws_kinesis_stream" "shared_services_destination_stream" {
   name             = "${var.prefix}-shared-services-log-destinion-stream"
   shard_count      = 1
-#   encryption_type  = "KMS"
-#   kms_key_id       = aws_kms_key.kinesis_stream_key.id
+  encryption_type  = "KMS"
+  kms_key_id       = aws_kms_key.kinesis_stream_key.id
   retention_period = 48
 
   shard_level_metrics = [
     "IncomingBytes",
-    "OutgoingBytes",
+    "OutgoingBytes"
   ]
 }
 
