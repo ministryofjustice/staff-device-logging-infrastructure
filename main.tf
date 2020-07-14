aa85019e6dbe8873eeb967c9d95ecc50546c1e39
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-14 14:28:02 +0100

    Rename refactor shared services log destination module

--- main.tf
+++ main.tf
@@ -190,7 +190,7 @@ module "firewall_roles" {
 }
 
 module "shared_services_log_destination" {
-  source                      = "./modules/shared_services_kinesis_firehose"
+  source                      = "./modules/shared_services_log_destination_stream"
   prefix                      = module.label.id
   region                      = data.aws_region.current_region.id
   shared_services_account_arn = data.aws_caller_identity.shared_services_account.account_id
