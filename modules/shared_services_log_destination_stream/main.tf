eb405f6c10c616036709c3da7c6dc8da6558528b
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-14 18:14:57 +0100

    Rename refactor to use underscores consistently

--- modules/shared_services_log_destination_stream/main.tf
+++ modules/shared_services_log_destination_stream/main.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "aws_kinesis_stream" "shared_services_destination_stream" {
   count            = local.shared_service_log_destination_count
-  name             = "${var.prefix}-shared-services-log-destination-stream"
+  name             = "${var.prefix}_shared_services_log_destination_stream"
   shard_count      = 1
   encryption_type  = "KMS"
   kms_key_id       = aws_kms_key.kinesis_stream_key.id
@@ -17,12 +17,12 @@ resource "aws_kinesis_stream" "shared_services_destination_stream" {
 }
 
 resource "aws_kms_key" "kinesis_stream_key" {
-  description             = "${var.prefix}-shared-services-kinesis-stream-key"
+  description             = "${var.prefix}_shared_services_kinesis_stream_key"
   deletion_window_in_days = 10
 }
 
-resource "aws_iam_role" "cloudwatch-to-kinesis-role" {
-  name = "${var.prefix}-cloudwatch-to-kinesis-role"
+resource "aws_iam_role" "cloudwatch_to_kinesis_role" {
+  name = "${var.prefix}_cloudwatch_to_kinesis_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -35,8 +35,9 @@ resource "aws_iam_role" "cloudwatch-to-kinesis-role" {
   })
 }
 
-resource "aws_iam_policy" "cloudwatch-to-kinesis-policy" {
-  name = "${var.prefix}-cloudwatch-to-kinesis-policy"
+resource "aws_iam_policy" "cloudwatch_to_kinesis_policy" {
+  count      = local.shared_service_log_destination_count
+  name = "${var.prefix}_cloudwatch_to_kinesis_policy"
   path = "/"
 
   policy = jsonencode({
@@ -45,7 +46,7 @@ resource "aws_iam_policy" "cloudwatch-to-kinesis-policy" {
       {
         "Effect": "Allow",
         "Action": "kinesis:PutRecord",
-        "Resource": "*"
+        "Resource": "${element(aws_kinesis_stream.shared_services_destination_stream.*.arn, 0)}"
       },
       {
         "Effect": "Allow",
@@ -61,26 +62,26 @@ resource "aws_iam_policy" "cloudwatch-to-kinesis-policy" {
       {
         "Effect": "Allow",
         "Action": "iam:PassRole",
-        "Resource": "${aws_iam_role.cloudwatch-to-kinesis-role.arn}"
+        "Resource": "${aws_iam_role.cloudwatch_to_kinesis_role.arn}"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild-attachment" {
+resource "aws_iam_role_policy_attachment" "cloudwatch_to_kinesis_attachment" {
   count      = local.shared_service_log_destination_count
-  role       = aws_iam_role.cloudwatch-to-kinesis-role.name
-  policy_arn = aws_iam_policy.cloudwatch-to-kinesis-policy.arn
+  role       = element(aws_iam_role.cloudwatch_to_kinesis_role.*.name, 0)
+  policy_arn = element(aws_iam_policy.cloudwatch_to_kinesis_policy.*.arn, 0)
 }
 
-resource "aws_cloudwatch_log_destination" "log-forward-to-kinesis" {
+resource "aws_cloudwatch_log_destination" "log_forward_to_kinesis" {
   count      = local.shared_service_log_destination_count
-  name       = "${var.prefix}-log-forward-to-kinesis"
-  role_arn   = aws_iam_role.cloudwatch-to-kinesis-role.arn
+  name       = "${var.prefix}_log_forward_to_kinesis"
+  role_arn   = aws_iam_role.cloudwatch_to_kinesis_role.arn
   target_arn = element(aws_kinesis_stream.shared_services_destination_stream.*.arn, 0)
 }
 
-data "aws_iam_policy_document" "log-forward-to-kinesis" {
+data "aws_iam_policy_document" "log_forward_to_kinesis" {
   count = local.shared_service_log_destination_count
   statement {
     effect = "Allow"
@@ -98,13 +99,13 @@ data "aws_iam_policy_document" "log-forward-to-kinesis" {
     ]
 
     resources = [
-      element(aws_cloudwatch_log_destination.log-forward-to-kinesis.*.arn, 0)
+      element(aws_cloudwatch_log_destination.log_forward_to_kinesis.*.arn, 0)
     ]
   }
 }
 
 resource "aws_cloudwatch_log_destination_policy" "cross_account_destination_policy" {
   count      = local.shared_service_log_destination_count
-  destination_name = element(aws_cloudwatch_log_destination.log-forward-to-kinesis.*.name, 0)
-  access_policy    = element(data.aws_iam_policy_document.log-forward-to-kinesis.*.json, 0)
+  destination_name = element(aws_cloudwatch_log_destination.log_forward_to_kinesis.*.name, 0)
+  access_policy    = element(data.aws_iam_policy_document.log_forward_to_kinesis.*.json, 0)
 }
