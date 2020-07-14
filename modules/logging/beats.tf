d175d751a0535ef466c63708bba4798290834899
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-14 14:21:32 +0100

    Re-instate security group rules for Lambdas

--- modules/logging/beats.tf
+++ modules/logging/beats.tf
@@ -56,9 +56,9 @@ resource "aws_security_group" "functionbeats" {
   vpc_id = var.vpc_id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
