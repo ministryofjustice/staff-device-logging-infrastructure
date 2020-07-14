e08f65c5b1b3eda8ec4e66d033f42d957b180209
Scott Edwards <scotteza@gmail.com>
2020-07-14 13:38:47 +0100

    Pull VPC flow logs  KMS key policy into its own file
    Co-authored-by: Caitlin <caitlin@madetech.com>

--- modules/vpc_flow_logs/main.tf
+++ modules/vpc_flow_logs/main.tf
@@ -13,46 +13,9 @@ resource "aws_flow_log" "pttp_flow_logs" {
 resource "aws_kms_key" "vpc_flow_logs_kms_key" {
   description             = "${var.prefix}-vpc-flow-logs-kms-key"
   deletion_window_in_days = 10
+  policy                  = data.template_file.vpc_flow_logs_kms_key_policies.rendered
 
   tags = var.tags
-
-  // TODO: put this policy into its own file
-  policy = <<POLICY
-{
- "Version": "2012-10-17",
-    "Id": "key-default-1",
-    "Statement": [
-        {
-            "Sid": "Enable IAM User Permissions",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-            },
-            "Action": "kms:*",
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "logs.${var.region}.amazonaws.com"
-            },
-            "Action": [
-                "kms:Encrypt*",
-                "kms:Decrypt*",
-                "kms:ReEncrypt*",
-                "kms:GenerateDataKey*",
-                "kms:Describe*"
-            ],
-            "Resource": "*",
-            "Condition": {
-                "ArnEquals": {
-                    "kms:EncryptionContext:aws:logs:arn": "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:*"
-                }
-            }
-        }    
-    ]
-}
-POLICY
 }
 
 resource "aws_kms_alias" "vpc_flow_logs_kms_key_alias" {
