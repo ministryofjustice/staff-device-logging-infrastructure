15f105b8b74d68342086cf4286cb40ea7f350c90
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-14 08:13:12 +0100

    Temporarily open SG on Lambda to access Kinesis

--- modules/logging/beats.tf
+++ modules/logging/beats.tf
@@ -115,9 +115,9 @@ resource "aws_security_group" "functionbeats" {
   vpc_id = var.vpc_id
 
   egress {
-    from_port   = 9200
-    to_port     = 9200
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
