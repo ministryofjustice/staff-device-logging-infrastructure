ddfd9acde94859b734185571925873e8964f8a55
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-20 12:44:30 +0100

    Re-instate tests

--- test/custom_log_gateway_test.go
+++ test/custom_log_gateway_test.go
@@ -15,170 +15,170 @@ import (
 	"time"
 )
 
-// const retryDelay = time.Second * 5
-// const retryCount = 20
-// const testRegion = "eu-west-2"
+const retryDelay = time.Second * 5
+const retryCount = 20
+const testRegion = "eu-west-2"
 
-// func TestLogCanBeReadFromQueue(t *testing.T) {
-// 	test := SetUpTest(t)
+func TestLogCanBeReadFromQueue(t *testing.T) {
+	test := SetUpTest(t)
 
-// 	defer CleaningUpUntilTheEndOf(test)
-// 	SpinUpTheModuleFor(test)
+	defer CleaningUpUntilTheEndOf(test)
+	SpinUpTheModuleFor(test)
 
-// 	WriteAMessageToTheApiAndExpect(200, withCorrectApiKeyFor(test), test)
-// 	VerifyThatMessageWasPlacedOnQueue(test)
-// }
+	WriteAMessageToTheApiAndExpect(200, withCorrectApiKeyFor(test), test)
+	VerifyThatMessageWasPlacedOnQueue(test)
+}
 
-// func TestLogCanBeSubmittedToAPIWithCorrectKey(t *testing.T) {
-// 	test := SetUpTest(t)
+func TestLogCanBeSubmittedToAPIWithCorrectKey(t *testing.T) {
+	test := SetUpTest(t)
 
-// 	defer CleaningUpUntilTheEndOf(test)
-// 	SpinUpTheModuleFor(test)
+	defer CleaningUpUntilTheEndOf(test)
+	SpinUpTheModuleFor(test)
 
-// 	WriteAMessageToTheApiAndExpect(200, withCorrectApiKeyFor(test), test)
-// }
+	WriteAMessageToTheApiAndExpect(200, withCorrectApiKeyFor(test), test)
+}
 
-// func TestLogCannotBeSubmittedToApiWithoutApiKey(t *testing.T) {
-// 	test := SetUpTest(t)
+func TestLogCannotBeSubmittedToApiWithoutApiKey(t *testing.T) {
+	test := SetUpTest(t)
 
-// 	defer CleaningUpUntilTheEndOf(test)
-// 	SpinUpTheModuleFor(test)
+	defer CleaningUpUntilTheEndOf(test)
+	SpinUpTheModuleFor(test)
 
-// 	WriteAMessageToTheApiAndExpect(403, "", test)
-// }
+	WriteAMessageToTheApiAndExpect(403, "", test)
+}
 
-// func TestThatQueueHasServerSideEncryptionEnabled(t *testing.T) {
-// 	thisTest := SetUpTest(t)
+func TestThatQueueHasServerSideEncryptionEnabled(t *testing.T) {
+	thisTest := SetUpTest(t)
 
-// 	defer CleaningUpUntilTheEndOf(thisTest)
-// 	SpinUpTheModuleFor(thisTest)
+	defer CleaningUpUntilTheEndOf(thisTest)
+	SpinUpTheModuleFor(thisTest)
 
-// 	VerifyThatQueueEncryptionIsEnabled(thisTest)
-// }
+	VerifyThatQueueEncryptionIsEnabled(thisTest)
+}
 
-// func SpinUpTheModuleFor(test testInfo) {
-// 	terraform.InitAndApply(test.instance, test.config)
-// }
+func SpinUpTheModuleFor(test testInfo) {
+	terraform.InitAndApply(test.instance, test.config)
+}
 
-// func CleaningUpUntilTheEndOf(test testInfo) {
-// 	terraform.Destroy(test.instance, test.config)
-// }
+func CleaningUpUntilTheEndOf(test testInfo) {
+	terraform.Destroy(test.instance, test.config)
+}
 
-// func VerifyThatMessageWasPlacedOnQueue(thisTest testInfo) {
-// 	queueUrl := terraform.Output(thisTest.instance, thisTest.config, "custom_log_queue_url")
+func VerifyThatMessageWasPlacedOnQueue(thisTest testInfo) {
+	queueUrl := terraform.Output(thisTest.instance, thisTest.config, "custom_log_queue_url")
 
-// 	sess, _ := session.NewSession(&aws.Config{Region: aws.String(testRegion)})
+	sess, _ := session.NewSession(&aws.Config{Region: aws.String(testRegion)})
 
-// 	sqsService := sqs.New(sess)
+	sqsService := sqs.New(sess)
 
-// 	result, err := sqsService.ReceiveMessage(&sqs.ReceiveMessageInput{
-// 		AttributeNames: []*string{
-// 			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
-// 		},
-// 		MessageAttributeNames: []*string{
-// 			aws.String(sqs.QueueAttributeNameAll),
-// 		},
-// 		QueueUrl:            &queueUrl,
-// 		MaxNumberOfMessages: aws.Int64(1),
-// 		VisibilityTimeout:   aws.Int64(20), // 20 seconds
-// 		WaitTimeSeconds:     aws.Int64(20),
-// 	})
+	result, err := sqsService.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            &queueUrl,
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout:   aws.Int64(20), // 20 seconds
+		WaitTimeSeconds:     aws.Int64(20),
+	})
 
-// 	assert.NoError(thisTest.instance, err)
-// 	assert.Len(thisTest.instance, result.Messages, 1, "***Received no messages***")
+	assert.NoError(thisTest.instance, err)
+	assert.Len(thisTest.instance, result.Messages, 1, "***Received no messages***")
 
-// 	expectedMessageBodyBytes, _ := json.Marshal(map[string]string{
-// 		"foo": "bar",
-// 	})
+	expectedMessageBodyBytes, _ := json.Marshal(map[string]string{
+		"foo": "bar",
+	})
 
-// 	expectedMessageBody := string(expectedMessageBodyBytes)
+	expectedMessageBody := string(expectedMessageBodyBytes)
 
-// 	messageBody := reformatJsonString(*result.Messages[0].Body, thisTest)
+	messageBody := reformatJsonString(*result.Messages[0].Body, thisTest)
 
-// 	assert.Equal(thisTest.instance, expectedMessageBody, messageBody)
-// }
+	assert.Equal(thisTest.instance, expectedMessageBody, messageBody)
+}
 
-// func VerifyThatQueueEncryptionIsEnabled(thisTest testInfo) {
-// 	sess, _ := session.NewSession(&aws.Config{Region: aws.String(testRegion)})
+func VerifyThatQueueEncryptionIsEnabled(thisTest testInfo) {
+	sess, _ := session.NewSession(&aws.Config{Region: aws.String(testRegion)})
 
-// 	sqsService := sqs.New(sess)
+	sqsService := sqs.New(sess)
 
-// 	queueUrl := terraform.Output(thisTest.instance, thisTest.config, "custom_log_queue_url")
+	queueUrl := terraform.Output(thisTest.instance, thisTest.config, "custom_log_queue_url")
 
-// 	kmsMasterKeyIdAttributeName := "KmsMasterKeyId"
+	kmsMasterKeyIdAttributeName := "KmsMasterKeyId"
 
-// 	requiredQueueAttributeNames := []*string{&kmsMasterKeyIdAttributeName}
+	requiredQueueAttributeNames := []*string{&kmsMasterKeyIdAttributeName}
 
-// 	queueAttributesInput := sqs.GetQueueAttributesInput{
-// 		AttributeNames: requiredQueueAttributeNames,
-// 		QueueUrl:       &queueUrl,
-// 	}
+	queueAttributesInput := sqs.GetQueueAttributesInput{
+		AttributeNames: requiredQueueAttributeNames,
+		QueueUrl:       &queueUrl,
+	}
 
-// 	queueAttributes, _ := sqsService.GetQueueAttributes(&queueAttributesInput)
+	queueAttributes, _ := sqsService.GetQueueAttributes(&queueAttributesInput)
 
-// 	if queueAttributes.Attributes[kmsMasterKeyIdAttributeName] == nil {
-// 		thisTest.instance.Errorf("***Queue does not have encryption enabled***")
-// 	}
-// }
+	if queueAttributes.Attributes[kmsMasterKeyIdAttributeName] == nil {
+		thisTest.instance.Errorf("***Queue does not have encryption enabled***")
+	}
+}
 
-// func WriteAMessageToTheApiAndExpect(code int, apiKey string, thisTest testInfo) {
-// 	loggingEndpointPath := terraform.Output(thisTest.instance, thisTest.config, "logging_endpoint_path")
+func WriteAMessageToTheApiAndExpect(code int, apiKey string, thisTest testInfo) {
+	loggingEndpointPath := terraform.Output(thisTest.instance, thisTest.config, "logging_endpoint_path")
 
-// 	requestBody, _ := json.Marshal(map[string]string{
-// 		"foo": "bar",
-// 	})
+	requestBody, _ := json.Marshal(map[string]string{
+		"foo": "bar",
+	})
 
-// 	_, err := http_helper.HTTPDoWithRetryE(thisTest.instance,
-// 		"POST",
-// 		loggingEndpointPath,
-// 		requestBody,
-// 		map[string]string{"Content-Type": "application/json", "X-API-KEY": apiKey},
-// 		code,
-// 		retryCount,
-// 		retryDelay,
-// 		nil,
-// 	)
+	_, err := http_helper.HTTPDoWithRetryE(thisTest.instance,
+		"POST",
+		loggingEndpointPath,
+		requestBody,
+		map[string]string{"Content-Type": "application/json", "X-API-KEY": apiKey},
+		code,
+		retryCount,
+		retryDelay,
+		nil,
+	)
 
-// 	assert.NoError(thisTest.instance, err, "***Api did not return code '%d'***", code)
-// }
+	assert.NoError(thisTest.instance, err, "***Api did not return code '%d'***", code)
+}
 
-// func withCorrectApiKeyFor(thisTest testInfo) string {
-// 	apiKey := terraform.Output(thisTest.instance, thisTest.config, "custom_logging_api_key")
-// 	return apiKey
-// }
+func withCorrectApiKeyFor(thisTest testInfo) string {
+	apiKey := terraform.Output(thisTest.instance, thisTest.config, "custom_logging_api_key")
+	return apiKey
+}
 
-// func reformatJsonString(theThing string, thisTest testInfo) string {
-// 	var messageBodyMap map[string]interface{}
-// 	err := json.Unmarshal([]byte(theThing), &messageBodyMap)
-// 	assert.NoError(thisTest.instance, err)
+func reformatJsonString(theThing string, thisTest testInfo) string {
+	var messageBodyMap map[string]interface{}
+	err := json.Unmarshal([]byte(theThing), &messageBodyMap)
+	assert.NoError(thisTest.instance, err)
 
-// 	messageBody, _ := json.Marshal(messageBodyMap)
+	messageBody, _ := json.Marshal(messageBodyMap)
 
-// 	return string(messageBody)
-// }
+	return string(messageBody)
+}
 
-// func SetUpTest(t *testing.T) testInfo {
-// 	t.Parallel()
+func SetUpTest(t *testing.T) testInfo {
+	t.Parallel()
 
-// 	uniqueId := random.UniqueId()
-// 	prefix := fmt.Sprintf("terratest-%s", uniqueId)
+	uniqueId := random.UniqueId()
+	prefix := fmt.Sprintf("terratest-%s", uniqueId)
 
-// 	rootFolder := ".."
-// 	terraformFolderRelativeToRoot := "modules/custom_logging_api"
+	rootFolder := ".."
+	terraformFolderRelativeToRoot := "modules/custom_logging_api"
 
-// 	//this is required to run the same example in parallel
-// 	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
+	//this is required to run the same example in parallel
+	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
 
-// 	return testInfo{
-// 		instance: t,
-// 		config: &terraform.Options{
-// 			TerraformDir: tempTestFolder,
-// 			Vars:         map[string]interface{}{"prefix": prefix, "region": testRegion},
-// 		},
-// 	}
-// }
+	return testInfo{
+		instance: t,
+		config: &terraform.Options{
+			TerraformDir: tempTestFolder,
+			Vars:         map[string]interface{}{"prefix": prefix, "region": testRegion},
+		},
+	}
+}
 
-// type testInfo struct {
-// 	instance *testing.T
-// 	config   *terraform.Options
-// }
+type testInfo struct {
+	instance *testing.T
+	config   *terraform.Options
+}
