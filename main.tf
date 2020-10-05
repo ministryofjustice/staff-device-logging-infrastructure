710972db7fe85285ee2dea2993853f09ef8292d7
Emile <emile.swarts@digital.justice.gov.uk>
2020-10-05 13:27:14 +0100

    Upgrade Terraform and modules
    This will allow new features and address security risks

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "> 0.12.0"
+  required_version = "0.13.4"
 
   backend "s3" {
     bucket         = "pttp-ci-infrastructure-client-core-tf-state"
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  version = "~> 2.68"
+  version = "~> 3.6"
   alias   = "env"
   assume_role {
     role_arn = var.assume_role
@@ -34,7 +34,7 @@ data "aws_caller_identity" "shared_services_account" {}
 
 module "label" {
   source  = "cloudposse/label/null"
-  version = "0.16.0"
+  version = "0.19.2"
 
   namespace = "pttp"
   stage     = terraform.workspace
