423c234b777ff7b7c0b984d3615db5caf4944162
Emile <emile.swarts@digital.justice.gov.uk>
2020-10-12 17:52:47 +0100

    Restore ingress security group rule on 443
    This is required for the containers to start.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -38,6 +38,13 @@ resource "aws_security_group" "ecr" {
   name   = "${var.prefix}-ecr"
   vpc_id = module.vpc.vpc_id
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = [var.cidr_block]
+  }
+
   egress {
     protocol    = "tcp"
     from_port   = 443
