30bac80547c94a7798c4188c9c256a41a841cae5
Emile <emile.swarts@digital.justice.gov.uk>
2020-10-15 19:02:09 +0100

    Add TCP Load Balancer listener

--- modules/syslog_endpoint/load_balancer.tf
+++ modules/syslog_endpoint/load_balancer.tf
@@ -50,3 +50,14 @@ resource "aws_lb_listener" "udp" {
     target_group_arn = aws_lb_target_group.target_group.arn
   }
 }
+
+resource "aws_lb_listener" "tcp" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = var.container_port
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
