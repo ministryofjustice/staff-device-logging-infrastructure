3ba05519ea459b1115c513bdefae0eb0035c0f6a
Emile <emile.swarts@digital.justice.gov.uk>
2020-10-16 14:12:02 +0100

    Subscribe to new log groups
    This log data will be shipped to the OST

--- main.tf
+++ main.tf
@@ -228,7 +228,10 @@ module "functionbeat_config" {
     "staff-device-${var.env}-dhcp-server-log-group",
     "/aws/rds/instance/staff-device-${var.env}-dhcp-db/audit",
     module.customLoggingApi.log_group_name,
-    module.syslog_endpoint.logging.log_group_name
+    module.syslog_endpoint.logging.log_group_name,
+    "/aws/vpc-flow-log/*",
+    "${module.label.id}-dhcp-admin-log-group",
+    "${module.label.id}-dns-server-log-group"
   ]
 
   syslog_log_groups = [
