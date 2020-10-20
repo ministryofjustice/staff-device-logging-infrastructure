73ad401026f483cdef33b5b11f0f808e58541a41
Emile <emile.swarts@digital.justice.gov.uk>
2020-10-20 17:08:24 +0100

    Alarm for provisioned concurrency spillover count on Lambda

--- modules/alarms/lambda.tf
+++ modules/alarms/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_metric_alarm" "invocations" {
   count               = length(var.lambda_function_names)
   alarm_name          = "${element(var.lambda_function_names, count.index)}-lambda"
   comparison_operator = "LessThanOrEqualToThreshold"
-  threshold           = "0"
+  threshold           = "1"
   evaluation_periods  = "1"
   metric_name         = "Invocations"
   namespace           = "AWS/Lambda"
@@ -38,4 +38,25 @@ resource "aws_cloudwatch_metric_alarm" "errors" {
 
   alarm_description = "Lambda errors for the ${element(var.lambda_function_names, count.index)} function"
   treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "provisioned_concurrency_spillover" {
+  count               = length(var.lambda_function_names)
+  alarm_name          = "${element(var.lambda_function_names, count.index)}-provisioned-concurrency-spillover-lambda"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = "5"
+  metric_name         = "ProvisionedConcurrencySpilloverInvocations"
+  namespace           = "AWS/Lambda"
+  statistic           = "Sum"
+  period              = "60"
+
+  dimensions = {
+    FunctionName = element(var.lambda_function_names, count.index)
+  }
+
+  alarm_actions = [aws_sns_topic.this.arn]
+
+  alarm_description = "Lambda provisioned concurrency spillover count for the ${element(var.lambda_function_names, count.index)} function"
+  treat_missing_data = "ignore"
 }
