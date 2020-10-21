c3639b3f1f8b6b6e77dc5c51fb84d2126227fea4
Emile <emile.swarts@digital.justice.gov.uk>
2020-10-21 16:57:13 +0100

    Block all public access to S3 CloudTrail bucket

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -66,3 +66,12 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
 
   tags = var.tags
 }
+
+resource "aws_s3_bucket_public_access_block" "cloudtrail_bucket_public_block" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
