9554eb8a55e9097e77b027903bfc2b00d5cf3c78
Emile <emile.swarts@digital.justice.gov.uk>
2020-10-23 15:55:34 +0100

    Rename syslog log group name

--- modules/syslog_endpoint/logging.tf
+++ modules/syslog_endpoint/logging.tf
@@ -5,7 +5,7 @@ resource "aws_cloudwatch_log_group" "server_log_group" {
 }
 
 resource "aws_cloudwatch_log_group" "syslog_log_group" {
-  name = "${var.prefix}-log-group"
+  name = "${var.prefix}-syslog"
 
   retention_in_days = 7
 }
