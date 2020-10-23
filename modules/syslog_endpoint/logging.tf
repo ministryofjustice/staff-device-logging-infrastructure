8e911344046873d58b1ab0b4c8da4425005c572e
Emile <emile.swarts@digital.justice.gov.uk>
2020-10-23 16:26:29 +0100

    Restore syslog log group name

--- modules/syslog_endpoint/logging.tf
+++ modules/syslog_endpoint/logging.tf
@@ -5,7 +5,7 @@ resource "aws_cloudwatch_log_group" "server_log_group" {
 }
 
 resource "aws_cloudwatch_log_group" "syslog_log_group" {
-  name = "${var.prefix}-syslog"
+  name = "${var.prefix}-log-group"
 
   retention_in_days = 7
 }
