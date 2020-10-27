41742c811a8c0949a82634179262cf3ef87fd2fe
Emile <emile.swarts@digital.justice.gov.uk>
2020-10-27 17:53:00 +0000

    Allow beats to call internal AWS services on port 80

--- modules/logging/beats.tf
+++ modules/logging/beats.tf
@@ -171,4 +171,11 @@ resource "aws_security_group" "functionbeats" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
