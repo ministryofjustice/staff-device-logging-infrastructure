9ba41d21cfcb2c6e9af7674642616e72f2bbf1c7
Emile <emile.swarts@digital.justice.gov.uk>
2020-11-04 09:26:52 +0000

    Make the Lambda error alarm less sensitive
    Network latency could mean that a message fails to ship the first time.
    Lambda retries when it has failed.  We don't want to be notified of a retry for one request.
    
    Bump this threshold to at least 5 errors over 2 minutes to establish
    whether it is an actual error.

--- modules/alarms/lambda.tf
+++ modules/alarms/lambda.tf
@@ -23,8 +23,8 @@ resource "aws_cloudwatch_metric_alarm" "errors" {
   count               = length(var.lambda_function_names)
   alarm_name          = "${element(var.lambda_function_names, count.index)}-lambda-invocations"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  threshold           = "1"
-  evaluation_periods  = "1"
+  threshold           = "5"
+  evaluation_periods  = "2"
   metric_name         = "Errors"
   namespace           = "AWS/Lambda"
   statistic           = "Sum"
