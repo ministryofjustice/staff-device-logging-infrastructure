789a147da444955b255959a3994a7dd19a97e3bf
Mitchell Dawson <mitchell.dawson@justice.gov.uk>
2022-01-06 16:02:51 +0000

    :bookmark: syslog endpoint logging tags

--- modules/syslog_endpoint/logging.tf
+++ modules/syslog_endpoint/logging.tf
@@ -2,11 +2,15 @@ resource "aws_cloudwatch_log_group" "server_log_group" {
   name = "${var.prefix}-server-log-group"
 
   retention_in_days = 7
+
+  tags = var.tags
 }
 
 resource "aws_cloudwatch_log_group" "syslog_log_group" {
   name = "${var.prefix}-logs-log-group"
 
   retention_in_days = 7
+
+  tags = var.tags
 }
 
